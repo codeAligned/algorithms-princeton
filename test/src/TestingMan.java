7bf1d39b60abea33ef282b35152a9b6fcb0c652b
tobias <tobiasleongzhunmun@gmail.com>
2019-02-16 18:19:31 +0800

    add TestingMan.class

---
+++ test/src/TestingMan.java
@@ -0,0 +1,11 @@
+public class TestingMan {
+    public String name;
+
+    public TestingMan(String var1) {
+        this.name = var1;
+    }
+
+    public String sayThat() {
+        return "my name is" + this.name;
+    }
+}
--- test/src/tester.java
+++ test/src/tester.java
@@ -5,5 +5,10 @@ public class tester {
     public static void main(String[] args) {
         StdOut.println("Hello world");
         StdOut.printf("Is your name %s?\n", args[0]);
+
+        TestingMan x = new TestingMan("kool");
+        String y = x.sayThat();
+
+        System.out.println(y);
     }
 }
