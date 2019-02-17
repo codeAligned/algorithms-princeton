3f38f7d86dc267f35b0884591b66c707078f5561
tobias <tobiasleongzhunmun@gmail.com>
2019-02-17 18:24:41 +0800

    complete Percolation.java

--- week1/src/Client.java
+++ week1/src/Client.java
@@ -2,7 +2,6 @@ public class Client {
 
     public static void main(String[] args) {
         Percolation p = new Percolation(10);
-        p.getGrid();
-        p.getGrid(3,2);
+
     }
 }
--- week1/src/Percolation.java
+++ week1/src/Percolation.java
@@ -1,28 +1,103 @@
-import java.util.Arrays;
+/**
+ * The Percolation class models a percolation system.
+ *
+ * @author Tobias Leong
+ */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    // grid
-    int[][] grid;
+    private WeightedQuickUnionUF uf;
+    private boolean[][] openGrid;
+    private int top = 0;
+    private int bottom;
+    private int n;
 
-    public Percolation(int n){
+
+
+    public Percolation(int inputN) {
+        n = inputN;
         if (n <= 0) {
             throw new IllegalArgumentException("n provided to initialise Percolation is less than or equal to zero.");
-        } else {
-            // this initializes to all zeroes upon calling
-            grid = new int[n][n];
+        }
+
+        // add two because of virtual top and bottom
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        openGrid = new boolean[n][n];
+        bottom = n * n + 1;
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (row <= 0 || col <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return openGrid[row - 1][col - 1];
+    }
+
+    private int getIndex(int i, int j){
+        return n * (i - 1 ) + j;
+    }
+
+    public void open(int row, int col) {
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new IllegalArgumentException();
+        }
+
+        // open the site
+        openGrid[row - 1][col - 1] = true;
+
+        // if top row, connect to top
+        if (row == 1) {
+            uf.union(getIndex(row, col), top);
+        }
+
+        // if bottom row, connect to bottom
+        if (row == n) {
+            uf.union(getIndex(row, col), bottom);
+        }
+
+        // check neighbours (T,B,L,R) and union
+        if (row > 1 && isOpen(row - 1, col)) {
+            uf.union(getIndex(row, col), getIndex(row - 1, col));
+        }
+
+        if (row < n && isOpen(row + 1, col)) {
+            uf.union(getIndex(row, col), getIndex(row + 1, col));
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            uf.union(getIndex(row, col), getIndex(row, col - 1));
+        }
+
+        if (col < n && isOpen(row, col + 1)) {
+            uf.union(getIndex(row, col), getIndex(row, col + 1));
         }
     }
 
-    // default getGrid returns entire array
-    public void getGrid() {
-        System.out.println(Arrays.deepToString(grid));
+    public boolean isFull(int row, int col) {
+        if (row <= 0 || col <= 0 || row > n || col > n) {
+            throw new IllegalArgumentException();
+        }
+
+        return uf.connected(getIndex(row, col), top);
+
     }
 
-    public void getGrid(int i, int j) {
-        if (i <= 0 || j <= 0) {
-            throw new IllegalArgumentException("i or j less than or equal zero.");
+    public int numberOfOpenSites() {
+        int sum = 0;
+
+        for (int i = 0; i < n; i++ ) {
+            for (int j = 0; j < n; j++) {
+                if (openGrid[i][j]) {
+                    sum++;
+                }
+            }
         }
-        System.out.println(grid[i][j]);
+        return sum;
+    }
+
+    public boolean percolates() {
+        return uf.connected(bottom, top);
     }
 }
