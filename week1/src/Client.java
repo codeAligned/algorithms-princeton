840eac4fc338314d9e4ad50a791cd1c2ff0440ce
tobias <tobiasleongzhunmun@gmail.com>
2019-02-17 17:13:51 +0800

    edit Percolation

---
+++ week1/src/Client.java
@@ -0,0 +1,8 @@
+public class Client {
+
+    public static void main(String[] args) {
+        Percolation p = new Percolation(10);
+        p.getGrid();
+        p.getGrid(3,2);
+    }
+}
--- week1/src/Percolation.java
+++ week1/src/Percolation.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class Percolation {
+
+    // grid
+    int[][] grid;
+
+    public Percolation(int n){
+        if (n <= 0) {
+            throw new IllegalArgumentException("n provided to initialise Percolation is less than or equal to zero.");
+        } else {
+            // this initializes to all zeroes upon calling
+            grid = new int[n][n];
+        }
+    }
+
+    // default getGrid returns entire array
+    public void getGrid() {
+        System.out.println(Arrays.deepToString(grid));
+    }
+
+    public void getGrid(int i, int j) {
+        if (i <= 0 || j <= 0) {
+            throw new IllegalArgumentException("i or j less than or equal zero.");
+        }
+        System.out.println(grid[i][j]);
+    }
 }
